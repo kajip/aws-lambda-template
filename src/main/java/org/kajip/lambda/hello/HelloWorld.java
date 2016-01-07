3d44d12011c7d042d68d7c82ef23fdf638c0e650
Tomomi Kajita <t-kajita@biglobe.co.jp>
2016-01-07 16:32:56 +0900

    Logging に Log4j が利用できるように修正
    Signed-off-by: Tomomi Kajita <t-kajita@biglobe.co.jp>

--- src/main/java/org/kajip/lambda/hello/HelloWorld.java
+++ src/main/java/org/kajip/lambda/hello/HelloWorld.java
@@ -1,14 +1,16 @@
 package org.kajip.lambda.hello;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloWorld {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     public String handler(String name, Context context) {
 
-        LambdaLogger logger = context.getLogger();
-        logger.log("received: " + name);
+        logger.debug("received: " + name);
 
         return "Hello, " + name;
     }
